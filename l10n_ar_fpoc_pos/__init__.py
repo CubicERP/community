d44dc1086771aa0dd7cb660420b98fe094ae5b06
lisbethgutierrez <lgutierrez@cubicerp.com>
2018-02-20 20:17:40 -0500

    [delete dir community]

--- l10n_ar_fpoc_pos/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import point_of_sale
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- product_prices_update/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import wizard_update_prices
